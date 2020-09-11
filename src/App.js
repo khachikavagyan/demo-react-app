c2753fd947fc8e3acf1183f18e97a7c81a766d4b
khachikavagyan <xavagyan777@gmail.com>
2020-09-11 19:19:49 +0400

    removed unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import './App.css';
-import Counter from './Demo/Counter';
 
 function App() {
   return (
     <div>
-      <Counter />
     </div>
   );
 }
--- src/test/index.js
+++
@@ -1,7 +0,0 @@
-import Example from '../Demo/Example';
-
-function sayHelo() {
-    console.log('Hello World!');
-}
-
-export default sayHelo;
