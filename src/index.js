27672ef935a3cfb40f656f94b3c3b5d4e3091308
khachikavagyan <xavagyan777@gmail.com>
2020-09-11 19:32:03 +0400

    tests commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+import AppTest from './App.test';
+AppTest();
 ReactDOM.render(
   <App />,
   document.getElementById('root')
